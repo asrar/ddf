6df01649698713b8d91ed55ec5b8e97eac555300
Asrar <asrarulhasan@gmail.com>
2022-12-15 14:57:46 +0500

    Initial commit

---
+++ lib/AppLayer/Overseer.dart
@@ -0,0 +1,34 @@
+
+class Overseer {
+  Map<dynamic, dynamic> repository = {};
+
+
+
+
+
+  static String userAction = "";
+  static int timeDelay = 0;
+
+
+  Overseer() {
+
+
+
+// register managers
+  }
+
+  static printWrapped(String text) {
+    final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
+    pattern.allMatches(text).forEach((match) => print(match.group(0)));
+  }
+
+  // register the manager to this overseer to store in repository
+  register(name, object) {
+    repository[name] = object;
+  }
+
+  // get the required manager from overseer when needed
+  fetch(name) {
+    return repository[name];
+  }
+}
